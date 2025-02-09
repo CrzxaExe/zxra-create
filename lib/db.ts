70503c7990c284a1743ce1b5e4660c5ede3fe05f
CrzxaExe <craserchannel@outlook.co.id>
2025-02-09 19:43:07 +0700

    fix db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -9,13 +9,16 @@ export const ConnectDB = async () => {
   try {
     if (defaultConnection) return defaultConnection;
 
-    const db = await mongoose.connect(mongo);
+    const db = await mongoose.connect(mongo, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     defaultConnection = db.connection;
     console.log("Connected to database");
+    return defaultConnection;
   } catch (error) {
-    console.error(error);
-
+    console.error("Error connecting to database:", error);
     throw error;
   }
 };
@@ -28,34 +31,35 @@ export const CloseDB = async () => {
     defaultConnection = null;
     console.log("Database closed");
   } catch (error) {
-    console.log(error);
+    console.error("Error closing database:", error);
   }
 };
 
 export const FindId = async (id: string) => {
   try {
-    await ConnectDB();
+    const connection = await ConnectDB();
 
     const data = await Account.findOne({ id });
 
-    if (!data) return { error: "There is no that id in database" };
+    if (!data) return { error: "There is no such id in database" };
     return data;
   } catch (error) {
-    console.log(error);
+    console.error("Error finding ID:", error);
   } finally {
     await CloseDB();
   }
 };
+
 export const FindEmail = async (email: string) => {
   try {
-    await ConnectDB();
+    const connection = await ConnectDB();
 
     const data = await Account.findOne({ email });
 
-    if (!data) return { error: "There is no that id in database" };
+    if (!data) return { error: "There is no such email in database" };
     return data;
   } catch (error) {
-    console.log(error);
+    console.error("Error finding email:", error);
   } finally {
     await CloseDB();
   }
