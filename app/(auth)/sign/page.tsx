5d77d8b52dbad70fe4feb20827758a3313b20df7
CrzxaExe <craserchannel@outlook.co.id>
2025-02-09 20:09:48 +0700

    lagi

--- app/(auth)/sign/page.tsx
+++ app/(auth)/sign/page.tsx
@@ -8,16 +8,16 @@ import SliderSign from "@/components/auth/sliderSign";
 import { metadata } from "@/app/layout";
 
 interface PageProps {
-  isLogin?: boolean;
+  isLoginSection?: boolean;
 }
 
-const Page: React.FC<PageProps> = ({ isLogin = false }) => {
+const Page: React.FC<PageProps> = ({ isLoginSection = false }) => {
   metadata.title = "Sign";
 
   return (
     <div className="w-full bg-base-upfloor/60 backdrop-blur-lg lg:rounded-xl max-w-screen-sm shadow-xl mt-1">
       <div className="grid grid-cols-2 gap-5 relative px-4 py-5">
-        <SliderSign isLogin={isLogin} />
+        <SliderSign isLogin={isLoginSection} />
         <div>
           <LoginForm />
           <div className="w-full">
